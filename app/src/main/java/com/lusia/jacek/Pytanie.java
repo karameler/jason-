976d9c8e91910558919c31e9638e76a94a934ee2
karameler <thebiggestbibarelyet@gmail.com>
2026-09-25 11:45:23 +0200

    konstruktor

--- app/src/main/java/com/lusia/jacek/Pytanie.java
+++ app/src/main/java/com/lusia/jacek/Pytanie.java
@@ -1,7 +1,9 @@
 package com.lusia.jacek;
 
 import com.google.gson.annotations.SerializedName;
-
+/*
+https://my-json-server.typicode.com/karameler/jaceklistapytan/pytania
+ */
 public class Pytanie {
     @SerializedName("pytanie")
     public String trescpytania;
@@ -13,4 +15,12 @@ public class Pytanie {
     public String OdpC;
     @SerializedName("odp")
     public int poprawnaodp;
+
+    public Pytanie(String trescpytania, String odpA, String odpB, String odpC, int poprawnaodp) {
+        this.trescpytania = trescpytania;
+        OdpA = odpA;
+        OdpB = odpB;
+        OdpC = odpC;
+        this.poprawnaodp = poprawnaodp;
+    }
 }
