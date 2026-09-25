7e9b5258cee184ce7155ac101bb2fcc652f73e9a
karameler <thebiggestbibarelyet@gmail.com>
2026-09-25 12:12:46 +0200

    do dokończenia nie dziala

--- app/src/main/java/com/lusia/jacek/MainActivity.java
+++ app/src/main/java/com/lusia/jacek/MainActivity.java
@@ -1,6 +1,7 @@
 package com.lusia.jacek;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Pytanie> pytania;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,29 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://my-json-server.typicode.com/karameler/jaceklistapytan/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        JsonPlaceHolderAPI jsonPlaceHolderAPI = retrofit.create(JsonPlaceHolderAPI.class);
+
+        Call<ArrayList<Pytanie>> call = jsonPlaceHolderAPI.getPytania();
+        call.enqueue(new Callback<ArrayList<Pytanie>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Pytanie>> call, Response<ArrayList<Pytanie>> response) {
+                if (!response.isSuccessful()){
+                    Toast.makeText(MainActivity.this, response.code(), Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                pytania = response.body();
+                Toast.makeText(MainActivity.this, pytania.get(0).trescpytania, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onFailure(Call<ArrayList<Pytanie>> call, Throwable t) {
+
+            }
         });
 
     }
